e6d52815325a1ae139962909392878c1cdd79968
amponsemmichael <amponsem_michael@yahoo.com>
2024-08-13 16:33:55 +0000

    feat(SPMS2): find product by id

--- src/main/java/com/amponsem/ecom_backend/controller/ProductController.java
+++ src/main/java/com/amponsem/ecom_backend/controller/ProductController.java
@@ -4,10 +4,8 @@ package com.amponsem.ecom_backend.controller;
 import com.amponsem.ecom_backend.model.Product;
 import com.amponsem.ecom_backend.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -27,4 +25,11 @@ public class ProductController {
     public List<Product> getProducts() {
        return productService.getAllProducts();
    }
+
+   @GetMapping("/product/{id}")
+    public Product getProduct(@PathVariable int id) {
+       return productService.getProductById(id);
+   }
+
+
 }
