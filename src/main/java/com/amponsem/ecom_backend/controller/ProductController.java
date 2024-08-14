929c9aafc32bfd14de8ba74539624299b56f6197
amponsemmichael <amponsem_michael@yahoo.com>
2024-08-14 11:28:11 +0000

    Feat(SPMS): update and delete a product

--- src/main/java/com/amponsem/ecom_backend/controller/ProductController.java
+++ src/main/java/com/amponsem/ecom_backend/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.DelegatingServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,40 +21,66 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-   @RequestMapping("/")
+    @RequestMapping("/")
     public String home() {
-       return "Hello World";
-   }
+        return "Hello World";
+    }
 
-   @GetMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts() {
-       return productService.getAllProducts();
-   }
+        return productService.getAllProducts();
+    }
 
-   @GetMapping("/product/{id}")
+    @GetMapping("/product/{id}")
     public Product getProduct(@PathVariable int id) {
-       return productService.getProductById(id);
-   }
+        return productService.getProductById(id);
+    }
 
-   @PostMapping("/product")
+    @PostMapping("/product")
     public ResponseEntity<?> addProduct(@RequestPart Product product,
                                         @RequestPart MultipartFile imageFile) {
-       try{
-           Product product1 = productService.addProduct(product, imageFile);
-           return new ResponseEntity<>(product1, HttpStatus.CREATED);
-       }
-       catch(Exception e){
-           return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-       }
-   }
+        try {
+            Product product1 = productService.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 
     @GetMapping("/product/{productId}/image")
     public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId) {
-       Product product = productService.getProductById(productId);
-       byte[] imageFile = product.getImageData();
+        Product product = productService.getProductById(productId);
+        byte[] imageFile = product.getImageData();
 
-       return ResponseEntity.ok()
-               .contentType(MediaType.valueOf(product.getImageType()))
-               .body(imageFile);
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile);
     }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile) {
+        Product product1 = null;
+        try {
+            product1 = productService.updateProduct(id, product, imageFile);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        if(product1 != null)
+            return new ResponseEntity<>("updated", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("not updated", HttpStatus.BAD_REQUEST);
+        }
+
+        @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id) {
+            Product product1 = productService.getProductById(id);
+            if(product1 != null){
+                productService.deleteProduct(id);
+                return new ResponseEntity<>("deleted", HttpStatus.OK);
+            }
+            else
+                return new ResponseEntity<>("not deleted", HttpStatus.BAD_REQUEST);
+        }
 }
+
--- src/main/java/com/amponsem/ecom_backend/repository/ProductRepo.java
+++ src/main/java/com/amponsem/ecom_backend/repository/ProductRepo.java
@@ -1,6 +1,5 @@
 package com.amponsem.ecom_backend.repository;
 
-
 import com.amponsem.ecom_backend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
