da6fc3c93de4795ccc5082ef6187b8a955498893
amponsemmichael <amponsem_michael@yahoo.com>
2024-08-20 11:09:31 +0000

    added spring security dependency to pom file to provide a login UI for a user to enter username and password

--- src/main/java/com/amponsem/ecom_backend/controller/ProductController.java
+++ src/main/java/com/amponsem/ecom_backend/controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.amponsem.ecom_backend.controller;
 
-
 import com.amponsem.ecom_backend.model.Product;
 import com.amponsem.ecom_backend.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +18,6 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello World";
-    }
 
     @GetMapping("/products")
     public List<Product> getProducts() {
